a83204ad661f718e8b6ee02c5581d3f95e353c88
Yuan Grund <yuan.grund@state.or.us>
2020-09-21 15:01:57 -0700

    Import data from Excel

--- app.R
+++ app.R
@@ -2,10 +2,11 @@ library(tidyverse)
 library(lubridate)
 library(plotly);library(ggplot2)
 library(shiny); library(shinythemes); library(shinyWidgets)
-library(leaflet)
+library(leaflet); library(leaflet.extras)
 library(DT)
 library(viridis)
 library(scales)
+library(rgdal); library(raster); library(sp)
 
 load("data.RData")
 
@@ -13,14 +14,16 @@ rm(ui); rm(server)
 
 ## app.R ----
 shinyApp(
-
+  
   ui <- fluidPage(
     
     theme = shinythemes::shinytheme("flatly"),
     shiny::navbarPage("HAB Data Visualizer",
                       
                       wellPanel(
-                        leaflet::leafletOutput("map")
+                        
+                        tags$style(type = "text/css", "#map {height: calc(80vh - 80px) !important;}"),
+                        leaflet::leafletOutput("map", width="100%",height="100%")
                       ),
                       
                       wellPanel(
@@ -30,8 +33,13 @@ shinyApp(
                             # _ Lakes ----
                             shinyWidgets::pickerInput(inputId = "lakes",
                                                       label = "Select a Waterbody:",
-                                                      choices = unique(sort(dta$GNISIDNAME)),
-                                                      selected = "Alkali Lake_01116863",
+                                                      choices = list(
+                                                        "Within Drinking Water Source Area" = 
+                                                          unique(sort(dta[which(dta$wi_DWSA == c("Yes")),]$GNISIDNAME)),
+                                                        "Not-Within Drinking Water Source Area" = 
+                                                          unique(sort(dta[which(dta$wi_DWSA == c("No")),]$GNISIDNAME))
+                                                      ),
+                                                      # selected = "Alkali Lake_01116863",
                                                       multiple = FALSE),
                             
                             # _ Date ----
@@ -49,11 +57,15 @@ shinyApp(
                             # _ Summary Statistics ----
                             checkboxGroupInput(inputId = "matrix",
                                                label = "Summary Statistics:",
-                                               choices = c("Mean" = "Mean",
-                                                           "Maximum" = "Maximum",
+                                               choices = c("Maximum" = "Maximum",
+                                                           "Mean" = "Mean",
                                                            "Minimum" = "Minimum"),
-                                               selected = "Mean")
+                                               selected = "Mean"),
                             
+                            # _ Plot types ----
+                            checkboxGroupInput(inputId = "plot_log",
+                                               label = "See Log Scale:",
+                                               choices = c("Log Scale" = "log"))
                           ),
                           
                           mainPanel(
@@ -78,8 +90,38 @@ shinyApp(
     output$map <- leaflet::renderLeaflet({
       
       leaflet::leaflet() %>% 
-        leaflet::addProviderTiles("Esri.WorldImagery",group = "Esir Satellite Map")
+        leaflet::addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+        leaflet::setView(lng = -122.1739, lat = 44.0582, zoom=7) %>% 
+        leaflet::addPolygons(data = study_lakes,
+                             color = "red",
+                             weight = 1,
+                             layer = ~study_lakes$Name,
+                             smoothFactor = 0.5,
+                             opacity = 1.0,
+                             fillOpacity = 0.5,
+                             highlightOptions = highlightOptions(color = "blue",
+                                                                 weight = 2,
+                                                                 bringToFront = TRUE),
+                             label = ~study_lakes$Name) %>% 
+        leaflet.extras::addResetMapButton() %>% 
+        leaflet::addMiniMap(position = "bottomleft",
+                            width = 300,
+                            height = 300,
+                            zoomLevelFixed = 5)
+    })
+    
+    # Observe click on shapes
+    observe({
       
+      click <- input$map_shape_click
+      if(is.null(click))
+        return()
+      else
+        leafletProxy("map") %>% 
+        setView(
+          lng = click$lng,
+          lat = click$lat,
+          zoom = 12)
     })
     
     # chart ----
@@ -95,11 +137,26 @@ shinyApp(
       
     })
     
+    type <- reactive({
+      
+      input$plot_log
+      
+    })
+    
+    yaxis <- reactive({
+      
+      if_else(length(input$plot_log)>0,
+              "Cyanobacteria (cells/mL)",
+              "Cyanobacteria (cells/mL)")
+      
+    })
+    
+    
     output$plot <- renderPlotly({
       
       plotly::plot_ly(
         data = df(),
-        x = ~Date,
+        x = ~as.Date(Date),
         y = ~`Cyanobacteria (cells/mL)`,
         split = ~`Summary Statistics`,
         type = "scatter",
@@ -107,10 +164,11 @@ shinyApp(
         color = ~`Summary Statistics`,
         colors = pal) %>% 
         plotly::layout(xaxis = list(title = "Date", range = c(min(df()$Date),max(df()$Date))),
-                       yaxis = list(title = "Cyanobacteria (cells/mL)"),
+                       # yaxis = list(title = "Cyanobacteria (cells/mL)"),
                        title = as.character(unique(df()$GNISIDNAME))
-        )
-      
+        ) %>% 
+        plotly::layout(yaxis = list(type = type(),
+                                    title = yaxis()))
       
     })
     
@@ -121,8 +179,11 @@ shinyApp(
     df_tbl <- reactive({
       
       df() %>% 
-        dplyr::select(GNISIDNAME,Date,`Cyanobacteria (cells/mL)`,`Summary Statistics`,`Within Drinking Water Source Area`) %>% 
-        dplyr::mutate(`Cyanobacteria (cells/mL)` = scales::comma(`Cyanobacteria (cells/mL)`))
+        dplyr::select(GNISIDNAME,Date,`Cyanobacteria (cells/mL)`,`Summary Statistics`) %>% 
+        dplyr::mutate(`Cyanobacteria (cells/mL)` = ifelse(as.character(`Cyanobacteria (cells/mL)`) == "6310", "Not Detected",
+                                                          scales::comma(`Cyanobacteria (cells/mL)`))) %>% 
+        dplyr::mutate(`Summary Statistics` = ifelse(as.character(`Cyanobacteria (cells/mL)`) == "Not Detected", "NA",
+                                                    `Summary Statistics`))
     })
     
     output$table <- DT::renderDataTable({
@@ -144,7 +205,7 @@ shinyApp(
         rownames = FALSE,
         filter = 'bottom'
       ) #%>% 
-        #DT::formatDate("Date","toLocaleString")
+      #DT::formatDate("Date","toLocaleString")
     }, server = FALSE)
     
     
--- data.R
+++ data.R
@@ -1,21 +1,54 @@
 library(tidyverse)
 library(lubridate)
+library(rgdal); library(raster); library(sp)
+library(readxl)
 
 setwd("//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/HAB_Shiny_app")
-# data <- read.csv("//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/HAB_Shiny_app/Data/data.csv")
 
-dta <- data %>% 
+# Plot and Table ----
+dta1 <- readxl::read_xlsx("//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/GIS/cyan/tables/HAB_resolvablelakes_2016_to_2019.xlsx",
+                          sheet = "HAB_resolvablelakes_2016_2020") %>% 
+  dplyr::mutate(wi_DWSA = NA)
+
+dta2 <- readxl::read_xlsx("//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/GIS/cyan/tables/HAB_resolvablelakes_toAug242020.xlsx",
+                          sheet = "HAB_resolvable_toAug242020") %>% 
+  dplyr::select(-"...13")
+
+dta3 <- readxl::read_xlsx("//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/GIS/cyan/tables/HAB_resolvablelakes_toAug242020.xlsx",
+                          sheet = "NHDWaterbody_resolvable_inDWSA")
+
+GNISNameID <- unique(sort(dta3$GNIS_Name_ID...1))
+
+dta <- rbind(dta1,dta2) %>% 
   dplyr::rename(Mean = MEAN_cellsml,
                 Maximum = MAX_cellsml,
                 Minimum = MIN_cellsml) %>% 
-  tidyr::gather(`Summary Statistics`, `Cyanobacteria (cells/mL)`, -GNISIDNAME,-COUNT,-AREA,-Day,-Year,-Date,-wi_DWSA) %>% 
+  tidyr::gather(`Summary Statistics`, `Cyanobacteria (cells/mL)`, -GNISIDNAME,-COUNT,-AREA,-PercentArea_Value, -Day,-Year,-Date,-wi_DWSA) %>% 
   tidyr::separate(GNISIDNAME,c("GNISNAME","GNISID"), sep="_") %>% 
-  dplyr::mutate(GNISIDNAME = paste0(GNISNAME,"-",GNISID)) %>% 
-  dplyr::mutate(Date = mdy(Date)) %>% 
-  dplyr::mutate(wi_DWSA = ifelse(wi_DWSA == "#N/A", "No",
-                                 ifelse(wi_DWSA == "","Unknown",
-                                        wi_DWSA))) %>% 
-  dplyr::rename(`Within Drinking Water Source Area` = wi_DWSA)
+  dplyr::mutate(GNISIDNAME = paste0(GNISNAME,"_",GNISID)) %>% 
+  dplyr::mutate(Date = lubridate::ymd(Date)) %>% 
+  dplyr::arrange(desc(Date)) %>% 
+  dplyr::mutate(wi_DWSA = ifelse(GNISIDNAME %in% GNISNameID, "Yes", "No"))
+
+# Map: shapefile ----
+lakes <- rgdal::readOGR(dsn = "//deqhq1/WQ-Share/Harmful Algal Blooms Coordination Team/GIS/cyan/NHDwaterbody_resolvable_lakes.shp",
+                        layer = "NHDwaterbody_resolvable_lakes",
+                        integer64="warn.loss",
+                        verbose = FALSE,
+                        stringsAsFactors = FALSE)
+# head(lakes) or head(lakes@data)
+# summary(lakes)
+
+study_lakes <- lakes@data %>% 
+  dplyr::mutate(zoom = ifelse(!is.na(GNIS_Name), "6.0", NA))
+
+study_lakes <- sp::spTransform(lakes,CRS("+proj=longlat +datum=WGS84"))
+
+# lake_names <- unique(sort(dta$GNISNAME))
+# study_lakes <- subset(lakes,lakes$NAME %in% c(lake_names))
+
+rm(data); rm(lakes); rm(lake_names)
+
 
 
 save.image(file = "data.RData")

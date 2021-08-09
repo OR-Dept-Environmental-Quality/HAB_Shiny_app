86d6170bce0586c1bb18e81b1b51977fabace4dd
Yuan Grund <yuan.grund@state.or.us>
2021-08-09 10:47:15 -0700

    data updated till 8/8/2021

---
+++ data/CyANconfig.py
@@ -0,0 +1,9 @@
+# CyANconfig.py
+
+baseurl = "https://oceancolor.gsfc.nasa.gov/CYAN/OLCI/"
+tif_file_path = '\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\HAB_Shiny_app\\data\\2021\\*.tif'
+extract_base = '\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\GIS\\cyan\\'
+zones = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\GIS\cyan\HAB_deschutes2020.gdb\NHDWaterbody_resolvable_lakes"
+dir_Shiny = "\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\HAB_Shiny_app\\data"
+themask = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\GIS\cyan\HAB_deschutes2020.gdb\stateline_buffer50albers"
+theextent = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\GIS\cyan\HAB_deschutes2020.gdb\stateline_buffer50web"

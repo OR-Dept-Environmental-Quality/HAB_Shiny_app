aaee11e252f2dad8cecdbaaeb3b9bc174a73b3f7
Yuan Grund <yuan.grund@state.or.us>
2024-03-18 11:54:44 -0700

    Initials for V5 Data

--- CyANwd.jl
+++
@@ -1,4 +0,0 @@
-# Specifies working directory for CyAN downloads
-
-working_dir = "C:\\Users\\dsobota\\Desktop\\Julia_CyANapp"
-# working_dir = "\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team"
--- io_connection.R
+++
@@ -1,7 +0,0 @@
-install.packages('rsconnect')
-
-rsconnect::setAccountInfo(name='deqygrund',
-                          token='EF48D214DCB7A89DE0BFE787ED359F04',
-                          secret='J9kLiGTX8A+2/SRkWB9ZwlrjXiEsHQAPffNKdtB0')
-
-library(rsconnect)
